7688c119a37c03e27925790d27c84f32419575fb
jsh <jangsh920626@gmail.com>
2024-11-08 13:32:38 +0900

    경로수정

--- src/App.js
+++ src/App.js
@@ -18,17 +18,17 @@ function App() {
         <Container>
           <a href="#home"><img src={process.env.PUBLIC_URL + "/img/hao_logo1.png"} style={{height:'70px',backgroundSize:'cover'}} alt="logo"/></a>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/bestSeller">BestSeller</Nav.Link>
-            <Nav.Link href="/new">New</Nav.Link>
-            <Nav.Link href="/steadySeller">SteadySeller</Nav.Link>
-            <Nav.Link onClick={()=>(navigate('/detail'))}>detail</Nav.Link>
+            <Nav.Link href="/react_shop">Home</Nav.Link>
+            <Nav.Link href="react_shop/bestSeller">BestSeller</Nav.Link>
+            <Nav.Link href="react_shop/new">New</Nav.Link>
+            <Nav.Link href="react_shop/steadySeller">SteadySeller</Nav.Link>
+            <Nav.Link onClick={()=>(navigate('react_shop/detail'))}>detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       <Routes>
-        <Route path="/" element={
+        <Route path="/react_shop" element={
           <>
             <div className="main-bg"></div>
             <div>
@@ -36,13 +36,13 @@ function App() {
             </div>
           </>
         }/>
-        <Route path="/detail" element={<Detail books={books}/>}/> 
+        <Route path="react_shop/detail" element={<Detail books={books}/>}/> 
         <Route path="*" element={<div>404</div>}/> 
-        <Route path="/about" element={<About/>}>
+        <Route path="react_shop/about" element={<About/>}>
           <Route path="member" element={<div>멤버</div>}/>
           <Route path="location" element={<About/>}/>
         </Route>
-        <Route path="/event" element={<Event/>}>
+        <Route path="react_shop/event" element={<Event/>}>
           <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}/>
           <Route path="two" element={<p>생일기념 쿠폰받기</p>}/>
         </Route>
