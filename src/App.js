86af0def39710baa49d324069e96a142ef11e4b3
jsh <jangsh920626@gmail.com>
2024-11-11 17:20:31 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
             <Nav.Link href="/bestSeller">BestSeller</Nav.Link>
             <Nav.Link href="/new">New</Nav.Link>
             <Nav.Link href="/steadySeller">SteadySeller</Nav.Link>
-            <Nav.Link onClick={()=>(navigate('/detail/:id'))}>detail</Nav.Link>
+            <Nav.Link onClick={()=>(navigate(`/react_shop/detail/0`))}>detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -48,7 +48,7 @@ function App() {
             </div>
           </>
         }/>
-        <Route path="react_shop/detail/:id" element={<Detail books={books}/>}/> {/**url 파라미터 */}
+        <Route path="/react_shop/detail/:id" element={<Detail books={books}/>}/> {/**url 파라미터 */}
         <Route path="*" element={<div>404</div>}/> 
         <Route path="/about" element={<About/>}>
           <Route path="member" element={<div>멤버</div>}/>
@@ -88,7 +88,7 @@ function Content(props) {
       {props.books.map((book,i)=>(
         <Col key={i}>
           <img src={process.env.PUBLIC_URL + `/img/book${book?.itemId}.PNG`} alt="" width={"90%"}/>
-          <h4><a href={`/detail/${book?.itemId}`}>{book?.title}</a></h4>
+          <h4><a href={`/react_shop/detail/${book?.itemId}`}>{book?.title}</a></h4>
           <p>{book?.price}</p>
         </Col>
       ))}
