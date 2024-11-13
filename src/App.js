0833034e83ae520cf298c445db77cfe3b2d3ae3a
jsh <jangsh920626@gmail.com>
2024-11-13 16:14:36 +0900

    상세페이지탭, 화면로드애니메이션효과추가

--- src/App.js
+++ src/App.js
@@ -1,131 +1,128 @@
 import { useState } from 'react';
-import {Spinner, Container, Nav, Navbar, Row, Col, Button} from 'react-bootstrap';
-import './App.module.css';
+import { Spinner, Container, Nav, Navbar, Row, Col, Button } from 'react-bootstrap';
 import data from './data.js' //data.js임시데이터
-import { Routes, Route, useNavigate, Outlet} from 'react-router-dom';
+import { Routes, Route, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './pages/Detail.js';
 import axios from 'axios' //axios 추가
+import './App.css'
 
 function App() {
 
-  const [books,setBooks] = useState(data) // 상품 state
-  const [clickCnt, setClickCnt] = useState(0) // 더보기 버튼 클릭 수 state
-  const [spin, setSpin] = useState(false) //로딩중ui state
+	const [books, setBooks] = useState(data) // 상품 state
+	const [clickCnt, setClickCnt] = useState(0) // 더보기 버튼 클릭 수 state
+	const [spin, setSpin] = useState(false) //로딩중ui state
 
-  const navigate = useNavigate()
-  
-  //가나다순 정렬
-  const handleSort=()=>{
-    let copy = [...books]
-    copy.sort((a,b)=>{ //sort함수
-      if (a.title < b.title) return -1 // a가 b보다 작으면 a가 먼저 오도록
-      if (a.title > b.title) return 1 // a가 b보다 크면 b가 먼저 오도록
-      return 0 // a와 b가 같으면 순서 유지
-    })
-    setBooks(copy)
-  }
+	const navigate = useNavigate()
 
-  return (
-    <div>
+	//가나다순 정렬
+	const handleSort = () => {
+		let copy = [...books]
+		copy.sort((a, b) => { //sort함수
+			if (a.title < b.title) return -1 // a가 b보다 작으면 a가 먼저 오도록
+			if (a.title > b.title) return 1 // a가 b보다 크면 b가 먼저 오도록
+			return 0 // a와 b가 같으면 순서 유지
+		})
+		setBooks(copy)
+	}
 
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <a href="/react_shop"><img src={process.env.PUBLIC_URL + "/img/hao_logo1.png"} style={{height:'70px',backgroundSize:'cover'}} alt="logo"/></a>
-          <Nav className="me-auto">
-            <Nav.Link href="/react_shop">Home</Nav.Link>
-            <Nav.Link href="/bestSeller">BestSeller</Nav.Link>
-            <Nav.Link href="/new">New</Nav.Link>
-            <Nav.Link href="/steadySeller">SteadySeller</Nav.Link>
-            <Nav.Link onClick={()=>(navigate(`/react_shop/detail/0`))}>detail</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+	return (
+		<div>
 
-      <Routes>
-        <Route path="/react_shop" element={
-          <>
-            <div className="main-bg"></div>
-            <div>
-              <Button variant="primary" onClick={handleSort}>정렬</Button>
-              <Content books={books}/>
-              {spin &&(<Spin/>)}
-              <Button variant="primary" onClick={()=>{
-                //로딩ui
-                setSpin(true)
-                setClickCnt(clickCnt+1)
-                axios.get(`https://codingapple1.github.io/shop/data${clickCnt+2}.json`)
-                .then((result)=>{
-                  const res = result.data
-                  const copy = [...books, ...res]             
-                  setBooks(copy)
-                  setSpin(false)
-                  //로딩ui 숨기기
-                })
-                .catch(()=>{
-                  console.log('실패')
-                  setSpin(false)
-                  //로딩ui 숨기기
-                })
-              }} disabled={clickCnt===2}>더보기</Button>
-            </div>
-          </>
-        }/>
-        <Route path="/react_shop/detail/:id" element={<Detail books={books}/>}/> {/**url 파라미터 */}
-        <Route path="/about" element={<About/>}>
-          <Route path="member" element={<div>멤버</div>}/>
-          <Route path="location" element={<About/>}/>
-        </Route>
-        <Route path="/event" element={<Event/>}>
-          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}/>
-          <Route path="two" element={<p>생일기념 쿠폰받기</p>}/>
-        </Route>
-        <Route path="*" element={<div>404</div>}/> 
-      </Routes>
-    </div>
-  );
+			<Navbar bg="light" data-bs-theme="light">
+				<Container>
+					<a href="/react_shop"><img src={process.env.PUBLIC_URL + "/img/hao_logo1.png"} style={{ height: '70px', backgroundSize: 'cover' }} alt="logo" /></a>
+					<Nav className="me-auto">
+						<Nav.Link href="/react_shop">Home</Nav.Link>
+						<Nav.Link href="/bestSeller">BestSeller</Nav.Link>
+						<Nav.Link href="/new">New</Nav.Link>
+						<Nav.Link href="/steadySeller">SteadySeller</Nav.Link>
+						<Nav.Link onClick={() => (navigate(`/react_shop/detail/0`))}>detail</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+
+			<Routes>
+				<Route path="/react_shop" element={
+					<>
+						<div className="main-bg"></div>
+						<div>
+							<Button variant="primary" onClick={handleSort}>정렬</Button>
+							<Content books={books} />
+							{spin && (<Spin />)}
+							<Button variant="primary" onClick={() => {
+								setSpin(true) //로딩ui show
+								setClickCnt(clickCnt + 1)
+								axios.get(`https://codingapple1.github.io/shop/data${clickCnt + 2}.json`)
+									.then((result) => {
+										const res = result.data
+										const copy = [...books, ...res]
+										setBooks(copy)
+										setSpin(false) //로딩ui 숨기기
+									})
+									.catch(() => {
+										console.log('실패')
+										setSpin(false) //로딩ui 숨기기
+									})
+							}} disabled={clickCnt === 2}>더보기</Button>
+						</div>
+					</>
+				} />
+				<Route path="/react_shop/detail/:id" element={<Detail books={books} />} /> {/**url 파라미터 */}
+				<Route path="/about" element={<About />}>
+					<Route path="member" element={<div>멤버</div>} />
+					<Route path="location" element={<About />} />
+				</Route>
+				<Route path="/event" element={<Event />}>
+					<Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+					<Route path="two" element={<p>생일기념 쿠폰받기</p>} />
+				</Route>
+				<Route path="*" element={<div>404</div>} />
+			</Routes>
+		</div>
+	);
 }
 
-function Event(){
-  return(
-    <div>
-      <h4>오늘의 이벤트</h4>
-      <Outlet/>
-    </div>
-  )
+function Event() {
+	return (
+		<div>
+			<h4>오늘의 이벤트</h4>
+			<Outlet />
+		</div>
+	)
 }
 
-function About(){
-  return(
-    <div>
-      <h4>회사정보임</h4>
-      <Outlet/>
-    </div>
-  )
+function About() {
+	return (
+		<div>
+			<h4>회사정보임</h4>
+			<Outlet />
+		</div>
+	)
 }
 
 function Content(props) {
-  return(
-    <Container>
-    <Row>
-      {props.books.map((book,i)=>(
-        <Col key={i}>
-          <img src={process.env.PUBLIC_URL + `/img/book${book?.id}.PNG`} alt="" width={"90%"}/>
-          <h4><a href={`/react_shop/detail/${book?.id}`}>{book?.title}</a></h4>
-          <p>{book?.price}</p>
-        </Col>
-      ))}
-    </Row>
-  </Container>
-  )
-  
+	return (
+		<Container>
+			<Row>
+				{props.books.map((book, i) => (
+					<Col key={i}>
+						<img src={process.env.PUBLIC_URL + `/img/book${book?.id}.PNG`} alt="" width={"90%"} />
+						<h4><a href={`/react_shop/detail/${book?.id}`}>{book?.title}</a></h4>
+						<p>{book?.price}</p>
+					</Col>
+				))}
+			</Row>
+		</Container>
+	)
+
 }
 
-function Spin(){
-  return(
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
-  )
+function Spin() {
+	return (
+		<Spinner animation="border" role="status">
+			<span className="visually-hidden">Loading...</span>
+		</Spinner>
+	)
 }
 
 export default App;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,69 +1,104 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { InputGroup, Form} from 'react-bootstrap';
+import { InputGroup, Form, Nav } from 'react-bootstrap';
+
+function Detail(props) {
+
+	const { id } = useParams() //url파라미터에 담긴 값을 가져오는 hook
+	const [alertVisible, setAlertVisible] = useState(true) //알림div 상태
+	const [inputVal, setInputVal] = useState('') //input 상태
+	const [inputAlert, setInputAlert] = useState(false) //알림div 상태
+	const [tabVisible, setTabVisible] = useState(0) //탭 상태 0번째,1번째,2번째
+	const [viewEffect, setViewEffect] = useState('') //화면로드시 상태
 
 
-function Detail(props){
+	//[id]번째 상품이아닌 상품id가 {id}인거 보여주기
+	const goods = props.books.find(function (x) {
+		return x.id === Number(id)
+	})
 
-    const {id} = useParams() //url파라미터에 담긴 값을 가져오는 hook
-    const [alertVisible, setAlertVisible]= useState(true) //알림div 상태
-    const [inputVal, setInputVal] = useState('') //input 상태
-    const [inputAlert, setInputAlert] = useState(false) //알림div 상태
+	useEffect(() => {
+		//타이머
+		const timer = setTimeout(() => { setAlertVisible(false) }, 2000)
+		//화면로드시애니메이션
+		const load = setTimeout(() => { setViewEffect('end') }, 200)
+		return () => {
+			clearTimeout(timer, load) //clean up function
+		}
+	}, [])
 
-    //[id]번째 상품이아닌 상품id가 {id}인거 보여주기
-    const goods = props.books.find(function(x){
-        return x.id === Number(id)
-    })
+	useEffect(() => {
+		if (inputVal !== '' && /\d/.test(inputVal)) {
+			setInputAlert(true)
+		}
+	}, [inputVal])
 
-    useEffect(()=>{
-        //타이머
-        const timer = setTimeout(()=>{ setAlertVisible(false) }, 2000)
-        return ()=>{
-            clearTimeout(timer) //clean up function
-        }
-    },[])
-
-    useEffect(()=>{
-        if (inputVal !== '' && /\d/.test(inputVal)) {
-            setInputAlert(true)
-        }        
-    },[inputVal])
-    
-    return(
-        <div className="container">
-            {goods != null && (
-                <div className="row">
-                    {alertVisible &&(
-                        <div className="alert alert-warning">
-                            2초 이내 구매시 할인
-                        </div>
-                    )}
-                    <div className="col-md-6">
-                        <img src={process.env.PUBLIC_URL + `/img/book${goods.id}.PNG`} alt="상품이미지" width={"100%"}/>
-                    </div>
-                    <div className="col-md-6">
-                        <h4 className="pt-5">{goods.title}</h4>
-                        <p>{goods.content}</p>
-                        <p>{goods.price} 원</p>
-                        <button className="btn btn-danger">주문하기</button>
-                        {inputAlert &&(
-                            <div className="alert alert-warning">
-                                No 숫자
-                            </div>
-                        )}
-                        <InputGroup className="mb-3" style={{marginTop:'10px'}}>
-                            <Form.Control
-                            aria-label="Default"
-                            aria-describedby="inputGroup-sizing-default"
-                            onChange={(e)=>{setInputVal(e.target.value)}}
-                            />
-                        </InputGroup>
-                    </div>
-                </div>
-            )}
-            {goods == null && <div>없는상품</div>}
-        </div>
-    )
+	return (
+		<div className={'container start ' + viewEffect} >
+			{goods != null && (
+				<div>
+					<div className="row">
+						{alertVisible && (
+							<div className="alert alert-warning">
+								2초 이내 구매시 할인
+							</div>
+						)}
+						<div className="col-md-6">
+							<img src={process.env.PUBLIC_URL + `/img/book${goods.id}.PNG`} alt="상품이미지" style={{ width: '70%' }} />
+						</div>
+						<div className="col-md-6">
+							<h4 className="pt-5">{goods.title}</h4>
+							<p>{goods.content}</p>
+							<p>{goods.price} 원</p>
+							<button className="btn btn-danger">주문하기</button>
+							{inputAlert && (
+								<div className="alert alert-warning">
+									No 숫자
+								</div>
+							)}
+							<InputGroup className="mb-3" style={{ marginTop: '10px' }}>
+								<Form.Control
+									aria-label="Default"
+									aria-describedby="inputGroup-sizing-default"
+									onChange={(e) => { setInputVal(e.target.value) }}
+								/>
+							</InputGroup>
+						</div>
+					</div>
+					<Nav justify variant="tabs" defaultActiveKey="detail" className="mt-3">
+						<Nav.Item>
+							<Nav.Link eventKey="detail" onClick={() => { setTabVisible(0) }}>상세정보</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey="review" onClick={() => { setTabVisible(1) }}>리뷰</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey="qna" onClick={() => { setTabVisible(2) }}>Q&A</Nav.Link>
+						</Nav.Item>
+					</Nav>
+					<TabContent tabVisible={tabVisible} />
+				</div>
+			)}
+			{goods == null && <div>없는상품</div>}
+		</div>
+	)
 }
 
-export default Detail;
+function TabContent({ tabVisible }) {
+
+	const [fade, setFade] = useState('') //탭애니메이션
+
+	useEffect(() => {
+		const timer = setTimeout(() => { setFade('end') }, 200)
+		return () => {
+			clearTimeout(timer)
+			setFade('')
+		}
+	}, [tabVisible])
+
+	return (<div className={"start " + fade}>
+		{[<div>상세정보</div>, <div>리뷰</div>, <div>qna</div>][tabVisible]}
+	</div>)
+}
+
+export default Detail;      
