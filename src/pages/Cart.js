cd1e7504ba9d3d03e25fce95de18ed17561313cc
jsh <jangsh920626@gmail.com>
2024-11-14 14:03:08 +0900

    redux state변경

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,35 +1,36 @@
 import { Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { addCart } from "../store";
 
 function Cart() {
+
+	let states = useSelector((state) => state)
+	let dispatch = useDispatch() //redux state변경요청
+
 	return (
 		<div className="container">
 			<Table striped bordered hover>
 				<thead>
 					<tr>
-						<th>#</th>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Username</th>
+						<th>No</th>
+						<th>id</th>
+						<th>name</th>
+						<th>count</th>
+						<th>추가</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>1</td>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-					</tr>
-					<tr>
-						<td>3</td>
-						<td colSpan={2}>Larry the Bird</td>
-						<td>@twitter</td>
-					</tr>
+					{states.carts.map((cart, i) => (
+						<tr key={i}>
+							<td>{i + 1}</td>
+							<td>{cart.id}</td>
+							<td>{cart.name}</td>
+							<td>{cart.count}</td>
+							<td><button onClick={() => {
+								dispatch(addCart({ id: cart.id }))
+							}}>+</button></td>
+						</tr>
+					))}
 				</tbody>
 			</Table>
 		</div>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+let user = createSlice({
+	name: 'user',
+	initialState: 'kim',
+	reducers: {
+		changeName(state) {
+			return 'john' + state
+		}
+	}
+})
+export let { changeName } = user.actions
+
+let carts = createSlice({
+	name: 'carts',
+	initialState: [
+		{ id: 0, name: 'White and Black', count: 2 },
+		{ id: 2, name: 'Grey Yordan', count: 1 }
+	],
+	reducers: {
+		addCart(state, action) {
+			const { id } = action.payload;
+			return state.map(item => (
+				item.id === id
+					? { ...item, count: item.count + 1 }
+					: item
+			))
+		}
+	}
+})
+export let { addCart } = carts.actions
+
 
 export default configureStore({
-	reducer: {}
+	reducer: {
+		user: user.reducer,
+		carts: carts.reducer
+	}
 }) 
